b09d1b094683afeee7d1a246c568b82712a1691e
Shranal Kumar <Shranal.Kumar@z.co.nz>
2020-06-11 14:25:12 +1200

    Lecture 66 - Limiting Middleware Execution to POST Requests

--- Section 05 - Working With Express.js/app.js
+++ Section 05 - Working With Express.js/app.js
@@ -9,8 +9,8 @@ app.use('/add-product', (req, res, next) => {
     res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
 });
 
-app.use('/product', (req, res, next) => {
-    console.log(req.body.title);
+app.post('/product', (req, res, next) => {
+    console.log(req.body);
     res.redirect('/');
     
 });
