dd615faf5118791424f98cb358a01b811d71cee5
Shranal Kumar <Shranal.Kumar@z.co.nz>
2020-06-15 11:28:36 +1200

    Lecture 73 - Using a Helper Function for Navigation

--- Section 05 - Working With Express.js/routes/not-found.js
+++ Section 05 - Working With Express.js/routes/not-found.js
@@ -1,10 +1,12 @@
 const path = require('path');
 const express = require('express');
 
+const rootDir = require('../utils/path');
+
 const router = express.Router();
 
 router.use('/', (req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, '../', 'views', 'not-found.html'));
+    res.status(404).sendFile(path.join(rootDir, 'views', 'not-found.html'));
 });
 
 module.exports = router;
