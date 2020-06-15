b89362d082a098c2db18ee56c490715d9a73bfab
Shranal Kumar <Shranal.Kumar@z.co.nz>
2020-06-15 13:21:17 +1200

    Lecture 79 - Sharing Data Across Requests and Users

---
+++ Section 06 - Working with Dynamic Content and Adding Templating Engines/app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+const adminData = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+const notFound = require('./routes/not-found');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/admin', adminData.routes);
+app.use(shopRoutes);
+app.use(notFound);
+
+app.listen(3000);
---
+++ Section 06 - Working with Dynamic Content and Adding Templating Engines/routes/not-found.js
@@ -0,0 +1,12 @@
+const path = require('path');
+const express = require('express');
+
+const rootDir = require('../utils/path');
+
+const router = express.Router();
+
+router.use('/', (req, res, next) => {
+    res.status(404).sendFile(path.join(rootDir, 'views', '404.html'));
+});
+
+module.exports = router;
