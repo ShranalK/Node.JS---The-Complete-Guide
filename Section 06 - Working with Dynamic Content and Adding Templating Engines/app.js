a9844eb631d683dd007ec6775e7bfa5a0fd5551b
Shranal Kumar <Shranal.Kumar@z.co.nz>
2020-06-24 11:57:52 +1200

    Lecture 90 - Adding the Layout to Handlebars

--- Section 06 - Working with Dynamic Content and Adding Templating Engines/app.js
+++ Section 06 - Working with Dynamic Content and Adding Templating Engines/app.js
@@ -1,22 +1,29 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const path = require('path');
-const expressHbs = require('express-handlebars');
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+const expressHbs = require("express-handlebars");
 
-const adminData = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
-const notFound = require('./routes/not-found');
+const adminData = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+const notFound = require("./routes/not-found");
 
 const app = express();
 
-app.engine('hbs', expressHbs());
-app.set('view engine', 'hbs');
-app.set('views', 'views');
+app.engine(
+  "hbs",
+  expressHbs({
+    layoutsDir: "views/layouts/",
+    defaultLayout: "main-layout",
+    extname: "hbs",
+  })
+);
+app.set("view engine", "hbs");
+app.set("views", "views");
 
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/admin', adminData.routes);
+app.use("/admin", adminData.routes);
 app.use(shopRoutes);
 app.use(notFound);
 
