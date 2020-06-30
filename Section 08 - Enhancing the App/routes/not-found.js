c9fc1e45fa51c8d005802e6cd76c5c45735a1c40
Shranal Kumar <Shranal.Kumar@z.co.nz>
2020-07-01 11:34:39 +1200

    Lecture 106 - Creating the Shop Structure

---
+++ Section 08 - Enhancing the App/routes/not-found.js
@@ -0,0 +1,10 @@
+const path = require("path");
+const express = require("express");
+
+const errorController = require("../controllers/error");
+
+const router = express.Router();
+
+router.use("/", errorController.get404);
+
+module.exports = router;
