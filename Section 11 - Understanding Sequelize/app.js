06f70a715e503470be3d5fb5d9b9e0d9b9505c87
Shranal Kumar <Shranal.Kumar@z.co.nz>
2020-07-15 10:02:27 +1200

    Lecture 150 - Connecting to the Database

---
+++ Section 11 - Understanding Sequelize/app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+
+const db = require("./utils/database");
+
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+const notFound = require("./routes/not-found");
+
+const app = express();
+
+app.set("view engine", "ejs");
+app.set("views", "views");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/admin", adminRoutes);
+app.use(shopRoutes);
+app.use(notFound);
+
+app.listen(3000);
