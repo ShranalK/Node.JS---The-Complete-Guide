7e653fc7a404cadc6fa5be1dfa712554c62e6642
Shranal Kumar <Shranal.Kumar@z.co.nz>
2020-07-15 13:27:23 +1200

    Lecture 162 - Creating and Managing a Dummy User

--- Section 11 - Understanding Sequelize/app.js
+++ Section 11 - Understanding Sequelize/app.js
@@ -18,6 +18,17 @@ app.set("views", "views");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
+app.use((req, res, next) => {
+  User.findByPk(1)
+    .then((user) => {
+      req.user = user;
+      next();
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 app.use(notFound);
@@ -26,8 +37,18 @@ Product.belongsTo(User, { constraints: true, onDelete: "CASCADE" });
 User.hasMany(Product);
 
 sequelize
-  .sync({ force: true })
+  //   .sync({ force: true })
+  .sync()
   .then((result) => {
+    return User.findByPk(1);
+  })
+  .then((user) => {
+    if (!user) {
+      return User.create({ name: "Shranal", email: "test@test.com" });
+    }
+    return user;
+  })
+  .then((user) => {
     app.listen(3000);
   })
   .catch((err) => {
