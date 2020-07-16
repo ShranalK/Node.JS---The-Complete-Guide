df5a7ed45e49526f55143f8e4838796ce56d1e0c
Shranal Kumar <Shranal.Kumar@z.co.nz>
2020-07-16 12:51:58 +1200

    Lecture 185 - Fetching All Products

--- Section 12 - Working with NoSQL and Using MongoDB/app.js
+++ Section 12 - Working with NoSQL and Using MongoDB/app.js
@@ -3,7 +3,7 @@ const bodyParser = require("body-parser");
 const path = require("path");
 
 const adminRoutes = require("./routes/admin");
-// const shopRoutes = require("./routes/shop");
+const shopRoutes = require("./routes/shop");
 const notFound = require("./routes/not-found");
 
 const mongoConnect = require("./utils/database").mongoConnect;
@@ -29,7 +29,7 @@ app.use((req, res, next) => {
 });
 
 app.use("/admin", adminRoutes);
-// app.use(shopRoutes);
+app.use(shopRoutes);
 app.use(notFound);
 
 mongoConnect(() => {
