fb591d1b1289eebcbf752639ca0a94e4b79bebb6
Shranal Kumar <Shranal.Kumar@z.co.nz>
2020-07-16 12:00:29 +1200

    Lecture 180 - Creating the Database Connection

--- Section 12 - Working with NoSQL and Using MongoDB/app.js
+++ Section 12 - Working with NoSQL and Using MongoDB/app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const path = require("path");
 
-// const adminRoutes = require("./routes/admin");
+const adminRoutes = require("./routes/admin");
 // const shopRoutes = require("./routes/shop");
 // const notFound = require("./routes/not-found");
 
@@ -27,7 +27,7 @@ app.use((req, res, next) => {
   //   });
 });
 
-// app.use("/admin", adminRoutes);
+app.use("/admin", adminRoutes);
 // app.use(shopRoutes);
 // app.use(notFound);
 
