3e9aaa16da84b4c62705952ea059bb4522c8271f
Shranal Kumar <Shranal.Kumar@z.co.nz>
2020-07-22 15:05:53 +1200

    Lecture 217 - Adding and Using a User Model

--- Section 13 - Working with Mongoose/app.js
+++ Section 13 - Working with Mongoose/app.js
@@ -7,7 +7,7 @@ const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 const notFound = require("./routes/not-found");
 
-// const User = require("./models/user");
+const User = require("./models/user");
 
 const app = express();
 
@@ -17,16 +17,16 @@ app.set("views", "views");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
-// app.use((req, res, next) => {
-//   User.findById("5f10faa21ba123be773dacee")
-//     .then((user) => {
-//       req.user = new User(user.name, user.email, user.cart, user._id);
-//       next();
-//     })
-//     .catch((err) => {
-//       console.log(err);
-//     });
-// });
+app.use((req, res, next) => {
+  User.findById("5f17ac14d5a4fc417c671beb")
+    .then((user) => {
+      req.user = user;
+      next();
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
 
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
@@ -37,6 +37,18 @@ mongoose
     "mongodb+srv://shranal:Sonushinu13@cluster0.ghrgp.mongodb.net/shop?retryWrites=true&w=majority"
   )
   .then((result) => {
+    User.findOne().then((user) => {
+      if (!user) {
+        const user = new User({
+          name: "Shranal",
+          email: "test@test.com",
+          cart: {
+            items: [],
+          },
+        });
+        user.save();
+      }
+    });
     app.listen(3000);
   })
   .catch((err) => {
