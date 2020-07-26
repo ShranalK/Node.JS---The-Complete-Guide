30979482567746eda92c05af422a46e7a56f24a0
Shranal Kumar <Shranal.Kumar@z.co.nz>
2020-07-27 10:48:00 +1200

    Lecture 242 - Deleting a Cookie

--- Section 14 - Sessions and Cookies/controllers/auth.js
+++ Section 14 - Sessions and Cookies/controllers/auth.js
@@ -1,7 +1,6 @@
 const User = require("../models/user");
 
 exports.getLogin = (req, res, next) => {
-  console.log(req.session.isLoggedIn);
   res.render("auth/login", {
     path: "/login",
     pageTitle: "Login",
@@ -19,4 +18,11 @@ exports.postLogin = (req, res, next) => {
     .catch((err) => {
       console.log(err);
     });
+};
+
+exports.postLogout = (req, res, next) => {
+  req.session.destroy((err) => {
+    console.log(err);
+    res.redirect("/");
+  });
 };
