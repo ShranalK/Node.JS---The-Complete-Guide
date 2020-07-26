6435754a9fabf2fbe4e81061babbbca52ff9852a
Shranal Kumar <Shranal.Kumar@z.co.nz>
2020-07-27 09:54:31 +1200

    Lecture 240 - Using MongoDB to Store Sessions

--- Section 14 - Sessions and Cookies/app.js
+++ Section 14 - Sessions and Cookies/app.js
@@ -3,6 +3,7 @@ const bodyParser = require("body-parser");
 const path = require("path");
 const mongoose = require("mongoose");
 const session = require("express-session");
+const MongoDbStore = require("connect-mongodb-session")(session);
 
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
@@ -10,8 +11,14 @@ const authRoutes = require("./routes/auth");
 const notFound = require("./routes/not-found");
 
 const User = require("./models/user");
+const MONGDODB_URI =
+  "mongodb+srv://shranal:Sonushinu13@cluster0.ghrgp.mongodb.net/shop?w=majority";
 
 const app = express();
+const store = new MongoDbStore({
+  uri: MONGDODB_URI,
+  collection: "sessions",
+});
 
 app.set("view engine", "ejs");
 app.set("views", "views");
@@ -20,7 +27,12 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use(
-  session({ secret: "my secret", resave: false, saveUninitialized: false })
+  session({
+    secret: "my secret",
+    resave: false,
+    saveUninitialized: false,
+    store: store,
+  })
 );
 
 app.use((req, res, next) => {
@@ -40,9 +52,7 @@ app.use(authRoutes);
 app.use(notFound);
 
 mongoose
-  .connect(
-    "mongodb+srv://shranal:Sonushinu13@cluster0.ghrgp.mongodb.net/shop?retryWrites=true&w=majority"
-  )
+  .connect(MONGDODB_URI)
   .then((result) => {
     User.findOne().then((user) => {
       if (!user) {
