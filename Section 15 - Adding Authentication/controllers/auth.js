0ac4a3ab21f333781b6d19e0fa01ffc92b98c708
Shranal Kumar <Shranal.Kumar@z.co.nz>
2020-07-27 13:53:14 +1200

    Lecture 256 - Adding the Signin Functionality

--- Section 15 - Adding Authentication/controllers/auth.js
+++ Section 15 - Adding Authentication/controllers/auth.js
@@ -18,13 +18,30 @@ exports.getSignup = (req, res, next) => {
 };
 
 exports.postLogin = (req, res, next) => {
-  User.findById("5f17ac14d5a4fc417c671beb")
+  const email = req.body.email;
+  const password = req.body.password;
+
+  User.findOne({ email: email })
     .then((user) => {
-      req.session.isLoggedIn = true;
-      req.session.user = user;
-      req.session.save((err) => {
-        res.redirect("/");
-      });
+      if (!user) {
+        return res.redirect("/login");
+      }
+      bcrypt
+        .compare(password, user.password)
+        .then((doMatch) => {
+          if (doMatch) {
+            req.session.isLoggedIn = true;
+            req.session.user = user;
+            return req.session.save((err) => {
+              res.redirect("/");
+            });
+          }
+          res.redirect("/login");
+        })
+        .catch((err) => {
+          console.log(err);
+          res.redirect("/login");
+        });
     })
     .catch((err) => {
       console.log(err);
