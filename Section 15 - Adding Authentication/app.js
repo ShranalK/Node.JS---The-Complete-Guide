a26154fda2524b037640f6e879b660f4061d3c40
Shranal Kumar <Shranal.Kumar@z.co.nz>
2020-07-27 13:24:47 +1200

    Lecture 252 - Our Updated Project Status

---
+++ Section 15 - Adding Authentication/app.js
@@ -0,0 +1,76 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+const mongoose = require("mongoose");
+const session = require("express-session");
+const MongoDbStore = require("connect-mongodb-session")(session);
+
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+const authRoutes = require("./routes/auth");
+const notFound = require("./routes/not-found");
+
+const User = require("./models/user");
+const MONGDODB_URI =
+  "mongodb+srv://shranal:Sonushinu13@cluster0.ghrgp.mongodb.net/shop?w=majority";
+
+const app = express();
+const store = new MongoDbStore({
+  uri: MONGDODB_URI,
+  collection: "sessions",
+});
+
+app.set("view engine", "ejs");
+app.set("views", "views");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use(
+  session({
+    secret: "my secret",
+    resave: false,
+    saveUninitialized: false,
+    store: store,
+  })
+);
+
+app.use((req, res, next) => {
+  if (!req.session.user) {
+    return next();
+  }
+  User.findById(req.session.user._id)
+    .then((user) => {
+      req.user = user;
+      next();
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+app.use("/admin", adminRoutes);
+app.use(shopRoutes);
+app.use(authRoutes);
+app.use(notFound);
+
+mongoose
+  .connect(MONGDODB_URI)
+  .then((result) => {
+    User.findOne().then((user) => {
+      if (!user) {
+        const user = new User({
+          name: "Shranal",
+          email: "test@test.com",
+          cart: {
+            items: [],
+          },
+        });
+        user.save();
+      }
+    });
+    app.listen(3000);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
